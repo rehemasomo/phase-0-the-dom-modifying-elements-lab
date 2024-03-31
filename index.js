afa5c8fe57e3fe8312fc345faffb81a598d239a6
rehemasomo <rehemasomo30@gmail.com>
2024-03-31 13:13:05 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1> element
+newHeader.textContent = 'REHEMA-SOMO is the champion'; 
+
+// Append the new <h1> element to the document body
+document.body.appendChild(newHeader);
